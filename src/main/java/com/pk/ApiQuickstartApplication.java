4660056040762a618a80b0083198e6cf3a979867
Pallav Kothari <pallav.kothari@pallavkoth-ltm.internal.salesforce.com>
2017-04-02 10:09:13 -0700

    Initial commit

---
+++ src/main/java/com/pk/ApiQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQuickstartApplication.class, args);
+	}
+}
